39a1603284b8778d49678dab8e07b15575908a74
SimranjotChahal <chahalsimranjot82@gmail.com>
2024-06-03 05:46:47 -0400

    Updated MainPage and Navbar components for better user experience.

---
+++ giftlink-frontend/src/reportWebVitals.js
@@ -0,0 +1,10 @@
+// src/reportWebVitals.js
+export default function reportWebVitals(onPerfEntry) {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      import('web-vitals').then(({ getCLS, getFID, getLCP }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getLCP(onPerfEntry);
+      });
+    }
+  }
