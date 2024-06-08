e7a6e8d17baa7351d996ab22aa9032a8fd0d9d9e
SimranjotChahal <chahalsimranjot82@gmail.com>
2024-06-08 04:57:18 -0400

    Implemented Backend in Login Component

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,16 +1,66 @@
 import React, { useState } from 'react';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 
 function LoginPage() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
 
-    const handleLogin = async () => {
-        console.log("Inside handleLogin");
-    }
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
 
-        return (
+      const handleLogin = async (e) => {
+        e.preventDefault();
+        //api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            
+        method: 'POST',
+        
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+          },
+        
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        });
+        
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+    
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+            
+          setIsLoggedIn(true);
+            
+          navigate('/app');
+        } else {
+            
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+      }
+
+
+    return (
       <div className="container mt-5">
         <div className="row justify-content-center">
           <div className="col-md-6 col-lg-4">
@@ -38,6 +88,8 @@ function LoginPage() {
                      value={password}
                      onChange={(e) => setPassword(e.target.value)}
                  />
+
+                    <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
               </div>
 
               <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
